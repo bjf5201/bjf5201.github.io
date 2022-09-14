3019d40be60d6eea5504a3f5bacbc205bcca4bf7
Bethany <bethany.fannin@pearson.com>
2022-09-14 15:56:56 -0400

    Finish working hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
-const burger = document.getElementById('burger');
-const navList = document.getElementById('nav-list');
+const burger = document.getElementById("burger");
+const navList = document.getElementById("nav-list");
+const heroNav = document.querySelector(".hero-nav");
 
 function toggleClass() {
-  burger.classList.toggle("active");
-  navList.classList.toggle("active");
+  burger.classList.toggle("show");
+  navList.classList.toggle("show");
+  heroNav.classList.toggle("active");
 }
 
-burger.addEventListener('click', toggleClass);
+burger.addEventListener("click", toggleClass);
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  burger.classList.remove("show");
+  navList.classList.remove("show");
+  heroNav.classList.remove("active");
+}))
